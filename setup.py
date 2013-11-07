f4c08ff5e9e2d5d8e4452987ae18cb46ea64ef62
Kyle Barbary <kylebarbary@gmail.com>
2013-11-07 11:25:11 -0600

    update version to v0.3.0 for release

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ LICENSE = 'BSD'
 URL = 'http://sncosmo.readthedocs.org'
 
 #VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.3.dev'
+VERSION = '0.3.0'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
