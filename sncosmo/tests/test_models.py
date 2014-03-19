29a24e953e3a7b252a27348c15ab7247f9bd6baf
Kyle Barbary <kylebarbary@gmail.com>
2014-03-19 15:29:43 -0500

    add tests

--- sncosmo/tests/test_models.py
+++ sncosmo/tests/test_models.py
@@ -23,14 +23,23 @@ class TestTimeSeriesSource:
             assert_allclose(self.source.flux([1.], [2000.]), np.array([[a]]))
 
     def test_getsource(self):
-        # register the source
+        # register the source & retrieve it from the registry
         registry.register(self.source, name="testsource",
                           data_class=sncosmo.Source)
-        
-        # retrieve it from registry
         m = sncosmo.get_source("testsource")
 
-    # TODO: make sure these actually give correct answers.
     def test_bandflux(self):
-        self.source.bandflux("bessellb", 0.)
+        self.source.set(amplitude=1.0)
+        f = self.source.bandflux("bessellb", 0.)
+
+        # Correct answer
+        b = sncosmo.get_bandpass("bessellb")
+        ans = np.sum(b.trans * b.wave * b.dwave) / sncosmo.models.HC_ERG_AA
+        assert ans == f
+
+    def test_bandflux_shapes(self):
+        # Just check that these work.
         self.source.bandflux("bessellb", 0., zp=25., zpsys="ab")
+        self.source.bandflux("bessellb", [0.1, 0.2], zp=25., zpsys="ab")
+        self.source.bandflux(["bessellb", "bessellv"], [0.1, 0.2], zp=25.,
+                             zpsys="ab")
---
+++ sncosmo/tests/test_plotting.py
@@ -0,0 +1,20 @@
+# Licensed under a 3-clause BSD style license - see LICENSES
+
+import numpy as np
+from matplotlib.figure import Figure
+
+import sncosmo
+
+
+class TestPlotLC:
+    def setup_class(self):
+        # Create a TimeSeriesSource with a flat spectrum at all times.
+        phase = np.linspace(0., 100., 10)
+        wave = np.linspace(1000., 10000., 100)
+        flux = np.ones((len(phase), len(wave)), dtype=np.float)
+        source = sncosmo.TimeSeriesSource(phase, wave, flux)
+        self.model = sncosmo.Model(source=source)
+
+    def test_plotmodel(self):
+        fig = sncosmo.plot_lc(model=self.model, bands=['bessellb', 'bessellr'])
+        assert isinstance(fig, Figure)
