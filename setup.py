bc4dd838b7df2b8a6c8a6249fa860cda94c6ef83
Kyle Barbary <kylebarbary@gmail.com>
2014-10-30 12:54:45 -0700

    convert setup to use astropy-helpers

--- setup.py
+++ setup.py
@@ -5,7 +5,15 @@ import glob
 import os
 import sys
 
-import setuptools_bootstrap
+# Ensure that astropy_helpers is available. If not installed, use
+# the included ah_bootstrap module to download from PyPI for temporary 
+# installation. This option doesn't permanently install the package.
+try:
+    import astropy_helpers
+except ImportError:
+    import ah_bootstrap
+    ah_bootstrap.use_astropy_helpers(use_git=False, auto_upgrade=False)
+
 from setuptools import setup
 
 #A dirty hack to get around some early import/configurations ambiguities
@@ -15,11 +23,10 @@ else:
     import __builtin__ as builtins
 builtins._ASTROPY_SETUP_ = True
 
-import astropy
-from astropy.setup_helpers import (register_commands, adjust_compiler,
-                                   get_package_info, get_debug_option,
-                                   is_distutils_display_option)
-from astropy.version_helpers import get_git_devstr, generate_version_py
+from astropy_helpers.setup_helpers import (
+    register_commands, adjust_compiler, get_debug_option, get_package_info)
+from astropy_helpers.git_helpers import get_git_devstr
+from astropy_helpers.version_helpers import generate_version_py
 
 # Need a recursive glob to find all package data files if there are
 # subdirectories
@@ -31,7 +38,7 @@ def recursive_glob(basedir, pattern):
             matches.append(os.path.join(root, filename))
     return matches
 
-# Set affiliated package-specific settings
+# package-specific values
 PACKAGENAME = 'sncosmo'
 DESCRIPTION = 'Package for supernova cosmology based on astropy'
 LONG_DESCRIPTION = ''
@@ -40,7 +47,16 @@ AUTHOR_EMAIL = 'kylebarbary@gmail.com'
 LICENSE = 'BSD'
 URL = 'http://sncosmo.readthedocs.org'
 
-#VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
+# Get the long description from the package's docstring
+__import__(PACKAGENAME)
+package = sys.modules[PACKAGENAME]
+LONG_DESCRIPTION = package.__doc__
+
+# Store the package name in a built-in variable so it's easy
+# to get from other parts of the setup infrastructure
+builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
+
+# VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
 VERSION = '0.5.dev'
 
 # Indicates if this version is a release version
@@ -59,20 +75,17 @@ cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
 adjust_compiler(PACKAGENAME)
 
 # Freeze build information in version.py
-generate_version_py(PACKAGENAME, VERSION, RELEASE, get_debug_option())
+generate_version_py(PACKAGENAME, VERSION, RELEASE,
+                    get_debug_option(PACKAGENAME))
 
-# Treat everything in scripts except README.rst as a script to be installed
-#ignore_scripts = ['generate_example_data.py']
-#scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
-#           if (os.path.basename(fname) != 'README.rst' and
-#               fname[-1] != '~' and
-#               os.path.basename(fname) not in ignore_scripts)]
+# The current scripts in sncosmo are for developer use, not intended to
+# be installed.
 scripts = []
 
 # Get configuration information from all of the various subpackages.
 # See the docstring for setup_helpers.update_package_files for more
 # details.
-package_info = get_package_info(PACKAGENAME)
+package_info = get_package_info()
 
 # Add the project-global data
 data_files = recursive_glob(os.path.join(PACKAGENAME, 'data'), '*')
@@ -80,21 +93,13 @@ data_files.append(os.path.join(PACKAGENAME, 'tests', 'coveragerc'))
 data_files = [f[len(PACKAGENAME)+1:] for f in data_files]
 package_info['package_data'][PACKAGENAME] = data_files
 
-install_requires = ['numpy', 'scipy', 'astropy >= 0.3']
-setup_requires = []
-
-# Avoid installing setup_requires dependencies if the user just
-# queries for information
-if is_distutils_display_option():
-    setup_requires = []
 
 setup(name=PACKAGENAME,
       version=VERSION,
       description=DESCRIPTION,
       scripts=scripts,
       requires=['numpy', 'scipy', 'astropy'],
-      install_requires=install_requires,
-      setup_requires=setup_requires,
+      install_requires=['numpy', 'scipy', 'astropy'],
       provides=[PACKAGENAME],
       author=AUTHOR,
       author_email=AUTHOR_EMAIL,
@@ -108,10 +113,8 @@ setup(name=PACKAGENAME,
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Physics'
-      ],
+          'Topic :: Scientific/Engineering :: Physics'],
       cmdclass=cmdclassd,
       zip_safe=False,
       use_2to3=True,
-      **package_info
-      )
+      **package_info)
