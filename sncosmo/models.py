c866d7a464e3977cda95e71da82b32e21a207a5b
Rahul Biswas <rbiswas4@gmail.com>
2014-12-02 12:24:57 -0800

    Made changes to docs according to discussions.

--- sncosmo/models.py
+++ sncosmo/models.py
@@ -446,8 +446,8 @@ class TimeSeriesSource(Source):
     (amplitude) is the single free parameter of the model. It should be noted
     that while t and \lambda are in the rest frame of the object, the flux
     density is defined at redshift zero. This means that for objects with the
-    same intrinsic luminosity, the amplitude will be smaller for objects that
-    have higher redshift.
+    same intrinsic luminosity, the amplitude will be smaller for objects at
+     larger luminosity distances.
 
     Parameters
     ----------
