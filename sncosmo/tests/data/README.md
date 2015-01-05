9792364e3c1f634dbe38148ca0bf7d95d664167b
Kyle Barbary <kylebarbary@gmail.com>
2015-01-05 14:12:40 -0800

    add test for read_lc() with salt2 format

---
+++ sncosmo/tests/data/README.md
@@ -0,0 +1,9 @@
+Example files for I/O tests
+---------------------------
+
+- `dust_subsection` : Contains a subsection of the SFD map. The code
+  expects a specific filename, so the file is in a subdirectory to
+  make it clear that it is a subsection of the original file.
+
+- `salt2_example.dat` : Example `lc-03D4ag.list` from
+  http://supernovae.in2p3.fr/salt/doku.php?id=usage
--- sncosmo/tests/test_io.py
+++ sncosmo/tests/test_io.py
@@ -1,6 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSES
 from __future__ import print_function
 
+from os.path import dirname, join
+
 import numpy as np
 from numpy.testing import assert_allclose, assert_almost_equal
 from astropy.extern import six
@@ -59,3 +61,20 @@ def test_griddata_fits():
     assert_allclose(x0_in, x0)
     assert_allclose(x1_in, x1)
     assert_allclose(y_in, y)
+
+
+def test_read_salt2():
+    fname = join(dirname(__file__), "data", "salt2_example.dat")
+    data = sncosmo.read_lc(fname, format="salt2")
+
+    # Test a few columns
+    assert_allclose(data["Date"], [52816.54, 52824.59, 52795.59, 52796.59])
+    assert_allclose(data["ZP"], [27.091335, 27.091335, 25.913054, 25.913054])
+    assert np.all(data["Filter"] == np.array(["MEGACAM::g", "MEGACAM::g",
+                                              "MEGACAM::i", "MEGACAM::i"]))
+    assert np.all(data["MagSys"] == "VEGA")
+
+    # Test a bit of metadata
+    assert_allclose(data.meta["Z_HELIO"], 0.285)
+    assert_allclose(data.meta["RA"], 333.690959)
+    assert data.meta["z_source"] == "H"
