733d4270605e01f905fc0515012ca9110a43bcae
Brigitta Sipocz <bsipocz@gmail.com>
2016-11-24 19:49:26 +0000

    Don't use workaround for OrderedDict import

--- scripts/generate_example_data.py
+++ scripts/generate_example_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import sncosmo
-from astropy.utils import OrderedDict as odict
+from collections import OrderedDict as odict
 from astropy.table import Table
 
 
@@ -18,7 +18,7 @@ flux = model.bandflux(bands, times, zp=zp, zpsys=zpsys)
 fluxerr = (0.05 * np.max(flux)) * np.ones(40, dtype=np.float)
 flux += fluxerr * np.random.randn(40)
 
-data = Table(odict([('time', times), ('band', bands), ('flux', flux), 
+data = Table(odict([('time', times), ('band', bands), ('flux', flux),
                     ('fluxerr', fluxerr), ('zp', zp), ('zpsys', zpsys)]),
              meta=dict(zip(model.param_names, model.parameters)))
 
