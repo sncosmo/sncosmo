28b264e52dfa3cba9ef4394f91ce79280075134c
Kyle Boone <kboone@berkeley.edu>
2021-04-19 17:02:41 -0700

    Formatting fixes

--- sncosmo/tests/test_io.py
+++ sncosmo/tests/test_io.py
@@ -217,6 +217,7 @@ def test_write_lc_snana():
 def test_load_example_data():
     data = sncosmo.load_example_data()
 
+
 def test_load_example_spectrum_data():
     wave, flux, fluxerr = sncosmo.load_example_spectrum_data()
 
