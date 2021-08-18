8dc0fcf93b68d3b189375ef68e21bfb05ec4a140
Kyle Boone <kboone@berkeley.edu>
2021-08-18 16:05:31 -0700

    Use extension-helpers to compile cython code automatically

--- setup.py
+++ setup.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import numpy
-from Cython.Build import cythonize
-import os
-
 from setuptools import setup
-from setuptools.extension import Extension
+from extension_helpers import get_extensions
 
-# Extensions
-source_files = [os.path.join("sncosmo", "salt2utils.pyx")]
-include_dirs = [numpy.get_include()]
-extensions = [Extension("sncosmo.salt2utils", source_files,
-                        include_dirs=include_dirs)]
-extensions = cythonize(extensions)
-
-setup(use_scm_version=True, ext_modules=extensions)
+setup(use_scm_version=True, ext_modules=get_extensions())
