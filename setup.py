513527fda9b8462092162af78ad296947866e714
Kyle Boone <kboone@berkeley.edu>
2021-08-18 14:36:42 -0700

    Specify extension manually for now, extension-helpers wasn't working.

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import numpy
+from Cython.Build import cythonize
 import os
 import re
 
-from extension_helpers import get_extensions
-
 from setuptools import setup
+from setuptools.extension import Extension
 
+# Extensions
+source_files = [os.path.join("sncosmo", "salt2utils.pyx")]
+include_dirs = [numpy.get_include()]
+extensions = [Extension("sncosmo.salt2utils", source_files,
+                        include_dirs=include_dirs)]
+extensions = cythonize(extensions)
 
 # Synchronize version from code.
 VERSION = re.findall(r"__version__ = \"(.*?)\"",
                      open(os.path.join("sncosmo", "__init__.py")).read())[0]
 
-setup(version=VERSION, ext_modules=get_extensions())
+setup(version=VERSION, ext_modules=extensions)
