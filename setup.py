4a46c53caee3edddd61a486afb98cb8afa8900d4
Kyle Boone <kboone@berkeley.edu>
2021-08-20 13:24:34 -0700

    Drop dependency on extension-helpers

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import numpy
 import os
 import re
 
 from setuptools import setup
-from extension_helpers import get_extensions
+from setuptools.extension import Extension
 
 # Synchronize version from code.
 VERSION = re.findall(r"__version__ = \"(.*?)\"",
                      open(os.path.join("sncosmo", "__init__.py")).read())[0]
 
+# Cython extensions
+extensions = [
+    Extension(
+        "sncosmo.salt2utils",
+        sources=[os.path.join("sncosmo", "salt2utils.pyx")],
+        include_dirs=[numpy.get_include()],
+    )
+]
+
 setup(
     version=VERSION,
-    ext_modules=get_extensions()
+    ext_modules=extensions,
 )
