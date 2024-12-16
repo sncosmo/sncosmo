b4f5baa571b562515e5e00968cdeb8a6a55e7707
Jakob van Santen <jvansanten@gmail.com>
2024-12-16 09:58:41 +0100

    Use extraction_filter in download_dir if available
    See: https://docs.python.org/3.12/library/tarfile.html#extraction-filters
    
    Tar archives for builtins were always well-behaved, but raise TarFile.errorlevel in tests for paranoia's sake

--- sncosmo/tests/test_download_builtins.py
+++ sncosmo/tests/test_download_builtins.py
@@ -22,13 +22,30 @@ magsystems = [i['name'] for i in _MAGSYSTEMS.get_loaders_metadata()]
 sources = [(i['name'], i['version']) for i in _SOURCES.get_loaders_metadata()]
 
 
+@pytest.fixture
+def all_tarfile_errors_are_fatal():
+    """
+    Raise tarfile.FilterError to caller (raised if tarfile.data_filter actually
+    filters out any archive members)
+    """
+    import tarfile
+    errorlevel = tarfile.TarFile.errorlevel
+    try:
+        tarfile.TarFile.errorlevel = 2
+        yield
+    finally:
+        tarfile.TarFile.errorlevel = errorlevel
+
+
 @pytest.mark.might_download
+@pytest.mark.usefixtures("all_tarfile_errors_are_fatal")
 @pytest.mark.parametrize("name", bandpasses)
 def test_builtin_bandpass(name):
     sncosmo.get_bandpass(name)
 
 
 @pytest.mark.might_download
+@pytest.mark.usefixtures("all_tarfile_errors_are_fatal")
 @pytest.mark.parametrize("name", bandpass_interpolators)
 def test_builtin_bandpass_interpolator(name):
     interpolator = _BANDPASS_INTERPOLATORS.retrieve(name)
@@ -36,12 +53,14 @@ def test_builtin_bandpass_interpolator(name):
 
 
 @pytest.mark.might_download
+@pytest.mark.usefixtures("all_tarfile_errors_are_fatal")
 @pytest.mark.parametrize("name,version", sources)
 def test_builtin_source(name, version):
     sncosmo.get_source(name, version)
 
 
 @pytest.mark.might_download
+@pytest.mark.usefixtures("all_tarfile_errors_are_fatal")
 @pytest.mark.parametrize("name", magsystems)
 def test_builtin_magsystem(name):
     sncosmo.get_magsystem(name)
