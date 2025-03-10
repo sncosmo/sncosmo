a612cb7887bc18cb03ca08227732d4cf906a62f1
Michael Coughlin <michael.w.coughlin@gmail.com>
2025-03-10 13:55:38 -0500

    whitespace

--- sncosmo/builtins.py
+++ sncosmo/builtins.py
@@ -532,9 +532,9 @@ for filt in ['Red']:
                                 args=(relpath,), meta=tess_meta)
 
 # GALEX
-galex_meta = { 
-    'filterset': 'galex', 
-    'retrieved': '10 March 2025', 
+galex_meta = {
+    'filterset': 'galex',
+    'retrieved': '10 March 2025',
     'dataurl': ('http://svo2.cab.inta-csic.es/svo/theory/fps/getdata.php?'
                 'format=ascii&id=Misc'),
     'description': 'GALEX filters from SVO (includes filter and instrument)'
